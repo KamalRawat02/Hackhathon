5f0e46f33e1d88159d284f8d46aa36fb4c5e4ce0
Astitva877 <ankurpatle18@gmail.com>
2023-03-03 13:04:44 +0530

    added bottom tab code in root navigator

--- Educate/src/navigation/RootNavigator.tsx
+++ Educate/src/navigation/RootNavigator.tsx
@@ -3,10 +3,22 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignUpScreen from '../screens/SignUpScreen';
 import Dashboard from '../screens/Dashboard';
 import LogInScreen from '../screens/LogInScreen';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const RootNavigator = () => {
+  const BottomTabs = () => {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name='Dashboard' component={Dashboard} />
+        {/* <Tab.Screen name='Doubts' component={Doubts} />
+        <Tab.Screen name='Jobs' component={Jobs} />
+        <Tab.Screen name='Events' component={Events} /> */}
+      </Tab.Navigator>
+    )
+  }
+
   return (
     <Stack.Navigator>
       <Stack.Screen
