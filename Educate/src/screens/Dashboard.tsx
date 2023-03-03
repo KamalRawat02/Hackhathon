9af3a9c98d4a325766547b5640418f827e5fea53
Astitva024 <astitvashrivastava204@gmail.com>
2023-03-03 15:18:19 +0530

    made todo list

--- Educate/src/screens/Dashboard.tsx
+++ Educate/src/screens/Dashboard.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 import Auth from '@react-native-firebase/auth';
 import {useNavigation, StackActions} from '@react-navigation/native';
 import {KeyboardAvoidingView} from 'react-native';
@@ -8,7 +15,20 @@ import {Image, ScreenWidth} from '@rneui/base';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Feather from 'react-native-vector-icons/Feather';
 import Octicons from 'react-native-vector-icons/Octicons';
+import Task from '../components/Task';
 const Dashboard = ({navigation}) => {
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task]);
+    setTask('');
+    console.log(taskItems);
+  };
+  const completeTask = index => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
   return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -85,12 +105,17 @@ const Dashboard = ({navigation}) => {
         </View>
       </View>
       <View style={{flex: 0.25}}>
-        <Text style={{color: '#343333', fontSize: 26, fontWeight: '600',marginStart:'5%'}}>
+        <Text
+          style={{
+            color: '#343333',
+            fontSize: 26,
+            fontWeight: '600',
+            marginStart: '5%',
+          }}>
           What's New Today,
         </Text>
         <View
           style={{
-  
             flex: 0.9,
             justifyContent: 'center',
             alignItems: 'center',
@@ -98,14 +123,13 @@ const Dashboard = ({navigation}) => {
           <View
             style={{
               flex: 0.9,
-              backgroundColor:'#458BE7',
+              backgroundColor: '#458BE7',
               width: '92%',
               borderRadius: 13,
               flexDirection: 'row',
             }}>
             <View
               style={{
-               
                 flex: 0.5,
                 justifyContent: 'center',
                 alignItems: 'center',
@@ -113,10 +137,10 @@ const Dashboard = ({navigation}) => {
               <View
                 style={{
                   flex: 0.7,
-                  backgroundColor:'#458BE7',
-                  borderWidth:1,
+                  backgroundColor: '#458BE7',
+                  borderWidth: 1,
                   width: '50%',
-                  borderColor:'black',
+                  borderColor: 'black',
                   borderRadius: 60,
                   alignItems: 'center',
                   justifyContent: 'center',
@@ -126,22 +150,103 @@ const Dashboard = ({navigation}) => {
                 </Text>
               </View>
             </View>
-            <View style={{ flex: 0.5,justifyContent:'center',alignItems:'center'}}>
+            <View
+              style={{
+                flex: 0.5,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
               <Text style={{color: 'white', fontSize: 10}}>Attendance</Text>
               <Text style={{color: 'white', fontSize: 45}}>82%</Text>
-              <View style={{height: 30, width: 60,justifyContent:'center',alignItems:'center',backgroundColor:"#454747",borderRadius:10}}>
+              <View
+                style={{
+                  height: 30,
+                  width: 60,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: '#454747',
+                  borderRadius: 10,
+                }}>
                 <Text style={{color: 'white'}}>Score</Text>
               </View>
             </View>
           </View>
         </View>
       </View>
-      <View style={{}}>
-
-
+      <View style={{flex: 0.55}}>
+        <View style={{flex: 0.15}}>
+          <Text
+            style={{
+              color: '#343333',
+              fontSize: 20,
+              fontWeight: '500',
+              marginStart: '5%',
+            }}>
+            Things to do,
+          </Text>
+          <Text style={{color: '#343333', fontSize: 13, marginStart: '5%'}}>
+            update yourself for better
+          </Text>
+        </View>
+        <View style={{flex: 0.85}}>
+          <ScrollView>
+            <View style={styles.Item}>
+              {taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => completeTask(index)}>
+                    <Task text={item} />
+                  </TouchableOpacity>
+                );
+              })}
+            </View>
+          </ScrollView>
+        </View>
       </View>
 
-      <View style={{flex: 0.1, backgroundColor: 'pink'}}>
+      <View style={{flex: 0.075, flexDirection: 'row'}}>
+        <View
+          style={{
+            // backgroundColor: 'yellow',
+            flex: 0.7,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TextInput
+            style={{
+              height: '80%',
+              width: '95%',
+              backgroundColor: '#a4bbda',
+              borderRadius: 10,
+            }}
+            value={task}
+            onChangeText={text => setTask(text)}
+            placeholder="Write Task"
+          />
+        </View>
+        <View
+          style={{flex: 0.3, justifyContent: 'center', alignContent: 'center'}}>
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View
+              style={{
+                height: '87%',
+                width: '95%',
+                borderRadius: 10,
+                backgroundColor: '#4E5254',
+                marginTop: '2%',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text style={{color: 'white', fontSize: 15, fontWeight: '500'}}>
+                Add Task
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* <View style={{flex: 0.1, backgroundColor: 'pink'}}>
         <Text> Dashboard </Text>
         <TouchableOpacity
           style={{
@@ -159,7 +264,7 @@ const Dashboard = ({navigation}) => {
           }}>
           <Text style={{color: '#fff'}}>Logout</Text>
         </TouchableOpacity>
-      </View>
+      </View> */}
     </KeyboardAvoidingView>
   );
 };
@@ -175,5 +280,8 @@ const styles = StyleSheet.create({
     height: '95%',
     //elevation: 10,
     borderRadius: 50,
+  },
+  Item: {
+    marginHorizontal:'5%'
   },
 });
